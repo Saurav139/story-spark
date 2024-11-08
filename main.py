7bfbaebfc7eb734216e9e6f56dfa9b63635eba32
unknown <sauravkrishna1@gmail.com>
2024-11-08 09:23:30 -0800

    CORSMiddleware

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from backend.routes import router
 
 app = FastAPI()
+# Add CORS Middleware to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],   # Allows all HTTP methods
+    allow_headers=["*"],   # Allows all headers
+)
 
 # Include the router from routes.py
 app.include_router(router)
